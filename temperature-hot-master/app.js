bc1bb447c86190ac3a82c0b22c3b3d731da2d9a2
khalidhmdop <khalidhasanct@gmail.com>
2021-09-05 17:19:16 +0600

    done

---
+++ temperature-hot-master/app.js
@@ -0,0 +1,24 @@
+const API_KEY = `41ddf8403aaec6d5d7bb93dc6d254b3f`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('lead-text', temperature.weather[0].main)
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('scr', url);
+}
+
+
+
